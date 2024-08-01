db9b840415f2183684175d8000f5eb2c9b2a8d7c
MADMAN-Modding <mad@kmdtech.net>
2024-07-31 22:29:31 -0400

    Some initial stuff

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include <dpp/dpp.h>
+#include "include/private.hpp"
+#include <iostream>
+
+const std::string BOT_TOKEN = token;
+int main()
+{
+   dpp::cluster bot(BOT_TOKEN, dpp::i_default_intents | dpp::i_message_content);
+
+   bot.on_log(dpp::utility::cout_logger());
+
+   bot.on_slashcommand([](const dpp::slashcommand_t &event)
+      {
+      std::string command = event.command.get_command_name();
+         if (command == "anon") {
+            event.reply(std::get<std::string>(event.get_parameter("message")));
+         }
+      }
+   );
+
+   bot.on_ready([&bot](const dpp::ready_t &event)
+      {
+         bot.global_bulk_command_delete();
+         if (dpp::run_once<struct register_bot_commands>())
+         {
+            // Makes the commands
+            dpp::slashcommand anon_command("anon-msg", "Enter a message to be anonymously sent", bot.me.id);
+            
+            // Encoding commands
+            anon_command.add_option(
+                  dpp::command_option(dpp::co_string, "message", "The message to anonymously send", true));
+            
+            // Creates the commands 
+            bot.global_command_create(anon_command);
+         } });
+   bot.start(dpp::st_wait);
+
+   return 0;
+}
